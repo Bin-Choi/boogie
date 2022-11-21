df4dc09b49a82152847965dbef5f2571bbd02333
csb <tph5268@naver.com>
2022-11-21 10:08:56 +0900

    create boxoffice chart

--- frontend/src/components/BoxOffice.vue
+++ frontend/src/components/BoxOffice.vue
@@ -1,10 +1,52 @@
 <template>
-  <div></div>
+  <div>
+    <canvas id="myChart" width="500" height="400"></canvas>
+  </div>
 </template>
 
 <script>
+import Chart from 'chart.js'
+import axios from 'axios'
+
+const API = 'http://127.0.0.1:8000'
+
 export default {
   name: 'BoxOffice',
+  data() {
+    return {
+      datasets: null,
+    }
+  },
+  mounted() {
+    const ctx = document.getElementById('myChart')
+
+    axios({
+      method: 'get',
+      url: `${API}/movies/boxoffice/`,
+    })
+      .then((res) => {
+        this.datasets = res.data
+
+        const myChart = new Chart(ctx, {
+          type: 'line',
+          data: {
+            labels: ['월', '화', '수', '목', '금', '토', '일'],
+            datasets: this.datasets,
+          },
+          options: {
+            scales: {
+              y: {
+                beginAtZero: true,
+              },
+            },
+          },
+        })
+        myChart
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  },
 }
 </script>
 
