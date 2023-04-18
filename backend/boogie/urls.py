e74be877ce8639df91f361132ba0a8b81346ca84
Byongho96 <unlike96@gmail.com>
2023-04-19 00:07:20 +0900

    [BE] feat: url 엔드포인트 api로 통일

--- backend/boogie/urls.py
+++ backend/boogie/urls.py
@@ -5,10 +5,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('movies/', include('movies.urls')),
-    path('community/', include('community.urls')),
-    path('users/', include('accounts.urls')),
-    path('accounts/', include('dj_rest_auth.urls')),
-    path('accounts/signup/', include('dj_rest_auth.registration.urls')),
+    path('api/admin/', admin.site.urls),
+    path('api/movies/', include('movies.urls')),
+    path('api/community/', include('community.urls')),
+    path('api/users/', include('accounts.urls')),
+    path('api/accounts/', include('dj_rest_auth.urls')),
+    path('api/accounts/signup/', include('dj_rest_auth.registration.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
